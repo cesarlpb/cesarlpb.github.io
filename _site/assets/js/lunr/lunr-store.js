e753dd8542ced1d21cb86311f0925264203b9a31
César <cesarlpb@gmail.com>
2022-11-01 19:50:03 +0100

    Portfolio with Jekyll

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Hello, world 🐐",
+        "excerpt":"Hello, world!   Welcome to my new portfolio web.   More info about me in social links and Blue Goat Labs website.  ","categories": ["BlueGoatLabs"],
+        "tags": ["Hello world"],
+        "url": "/bluegoatlabs/hello-world/",
+        "teaser": "/assets/images/hello-world.png"
+      },{
+        "title": "Blue Goat Labs",
+        "excerpt":"What is Blue Goat Labs? Blue Goat Labs is a startup that provides all kind of digital services, from software development to specialized coaching. Custom software development Multiplatform Web development Web 3 integration Blockchain Solidity Smart contracts Supply chain Coaching Sales training Marketing Business development Who are We? A group...","categories": ["BGL"],
+        "tags": ["Hello world"],
+        "url": "/bgl/blue-goat-labs/",
+        "teaser": "/assets/images/bgl-banner.png"
+      }]
---
+++ assets/images/🐐 Blue Goat Labs 🐐 - Turnkey Software Solutions_files/main.0c5c8a48b374a0df8570.hot-update.js
@@ -0,0 +1,63 @@
+webpackHotUpdate("main",{
+
+/***/ "./src/components/Common/Logo.jsx":
+/*!****************************************!*\
+  !*** ./src/components/Common/Logo.jsx ***!
+  \****************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _home_ubuntu_dev_Blue_Goat_Labs_website_client_node_modules_babel_preset_react_app_node_modules_babel_runtime_helpers_esm_classCallCheck__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./node_modules/babel-preset-react-app/node_modules/@babel/runtime/helpers/esm/classCallCheck */ "./node_modules/babel-preset-react-app/node_modules/@babel/runtime/helpers/esm/classCallCheck.js");
+/* harmony import */ var _home_ubuntu_dev_Blue_Goat_Labs_website_client_node_modules_babel_preset_react_app_node_modules_babel_runtime_helpers_esm_createClass__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./node_modules/babel-preset-react-app/node_modules/@babel/runtime/helpers/esm/createClass */ "./node_modules/babel-preset-react-app/node_modules/@babel/runtime/helpers/esm/createClass.js");
+/* harmony import */ var _home_ubuntu_dev_Blue_Goat_Labs_website_client_node_modules_babel_preset_react_app_node_modules_babel_runtime_helpers_esm_possibleConstructorReturn__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/babel-preset-react-app/node_modules/@babel/runtime/helpers/esm/possibleConstructorReturn */ "./node_modules/babel-preset-react-app/node_modules/@babel/runtime/helpers/esm/possibleConstructorReturn.js");
+/* harmony import */ var _home_ubuntu_dev_Blue_Goat_Labs_website_client_node_modules_babel_preset_react_app_node_modules_babel_runtime_helpers_esm_getPrototypeOf__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./node_modules/babel-preset-react-app/node_modules/@babel/runtime/helpers/esm/getPrototypeOf */ "./node_modules/babel-preset-react-app/node_modules/@babel/runtime/helpers/esm/getPrototypeOf.js");
+/* harmony import */ var _home_ubuntu_dev_Blue_Goat_Labs_website_client_node_modules_babel_preset_react_app_node_modules_babel_runtime_helpers_esm_inherits__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./node_modules/babel-preset-react-app/node_modules/@babel/runtime/helpers/esm/inherits */ "./node_modules/babel-preset-react-app/node_modules/@babel/runtime/helpers/esm/inherits.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var _assets_images_logos_logo_png__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../../assets/images/logos/logo.png */ "./src/assets/images/logos/logo.png");
+/* harmony import */ var _assets_images_logos_logo_png__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(_assets_images_logos_logo_png__WEBPACK_IMPORTED_MODULE_6__);
+
+
+
+
+
+var _jsxFileName = "/home/ubuntu/dev/Blue-Goat-Labs-website/client/src/components/Common/Logo.jsx";
+
+ // blue
+var Logo = /*#__PURE__*/function (_React$Component) {
+  Object(_home_ubuntu_dev_Blue_Goat_Labs_website_client_node_modules_babel_preset_react_app_node_modules_babel_runtime_helpers_esm_inherits__WEBPACK_IMPORTED_MODULE_4__["default"])(Logo, _React$Component);
+  function Logo() {
+    Object(_home_ubuntu_dev_Blue_Goat_Labs_website_client_node_modules_babel_preset_react_app_node_modules_babel_runtime_helpers_esm_classCallCheck__WEBPACK_IMPORTED_MODULE_0__["default"])(this, Logo);
+    return Object(_home_ubuntu_dev_Blue_Goat_Labs_website_client_node_modules_babel_preset_react_app_node_modules_babel_runtime_helpers_esm_possibleConstructorReturn__WEBPACK_IMPORTED_MODULE_2__["default"])(this, Object(_home_ubuntu_dev_Blue_Goat_Labs_website_client_node_modules_babel_preset_react_app_node_modules_babel_runtime_helpers_esm_getPrototypeOf__WEBPACK_IMPORTED_MODULE_3__["default"])(Logo).apply(this, arguments));
+  }
+  Object(_home_ubuntu_dev_Blue_Goat_Labs_website_client_node_modules_babel_preset_react_app_node_modules_babel_runtime_helpers_esm_createClass__WEBPACK_IMPORTED_MODULE_1__["default"])(Logo, [{
+    key: "render",
+    value: function render() {
+      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_5___default.a.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_5___default.a.createElement("img", {
+        src: _assets_images_logos_logo_png__WEBPACK_IMPORTED_MODULE_6___default.a,
+        alt: "logo",
+        style: {
+          backgroundColor: 'transparent',
+          padding: '10px',
+          margin: '10px',
+          width: '150px'
+        },
+        __self: this,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 8,
+          columnNumber: 9
+        }
+      }));
+    }
+  }]);
+  return Logo;
+}(react__WEBPACK_IMPORTED_MODULE_5___default.a.Component);
+/* harmony default export */ __webpack_exports__["default"] = (Logo);
+
+/***/ })
+
+})
+//# sourceMappingURL=main.0c5c8a48b374a0df8570.hot-update.js.map
