1bae6d3ebd7cf61ea34167f0fe7240d9b44fb6fa
César <cesarlpb@gmail.com>
2023-05-25 18:59:11 +0200

    Bucle para escribir datos de idiomas

--- js/main.mjs
+++ js/main.mjs
@@ -1,3 +1,16 @@
+"use strict"
+
 import { idiomas } from './lang.js'
 
+
 console.log(idiomas)
+let el = document.getElementById("idiomas");
+let data = idiomas.es
+console.log(data)
+for(const d of data){
+  el.innerHTML += `<p class="mb-1">${d.idioma}</p>
+<div class="progress" style="height: 0.65rem;">
+  <div class="progress-bar" role="progressbar" style="width: ${d.nivel}%" aria-valuenow="${d.nivel}" aria-valuemin="0" aria-valuemax="100"></div>
+</div>`
+}
+  
