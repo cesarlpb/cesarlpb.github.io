ccbbc34e7ac6dd59dd4306ad2b60bbcdc295f9ce
César <cesarlpb@gmail.com>
2024-10-09 14:03:30 +0200

    update en proyectos

--- js/proyectos.js
+++ js/proyectos.js
@@ -7,6 +7,20 @@ export const proyectos = {
       url: "https://methodobitcoin.com/es/",
       tags: ["Django", "Python", "Cloud"]
     },
+    {
+      tipo: "profesional",
+      titulo: "Face Recognition demo", 
+      descripcion: "Demo donde se aplica filtro en una foto de prueba a partir de hasta 3 fotos de referencia",
+      url: "https://demo.codersforyou.es/",
+      tags: ["Django", "Python", "Cloud", "face-recognition"]
+    },
+    {
+      tipo: "profesional",
+      titulo: "App legal demo", 
+      descripcion: "App de jurisprudencia",
+      url: "https://demo2.codersforyou.es/accounts/login/?next=/",
+      tags: ["Django", "Python", "Cloud"]
+    },
   ],
   en: [
     {
@@ -16,5 +30,20 @@ export const proyectos = {
       url: "https://methodobitcoin.com/es/",
       tags: ["Django", "Python", "Cloud"]
     },
+    {
+      tipo: "profesional",
+      titulo: "Face Recognition demo", 
+      descripcion: "Upload up to 3 reference pictures to apply filter on a test picture",
+      url: "https://demo.codersforyou.es/",
+      tags: ["Django", "Python", "Cloud", "face-recognition"]
+    },
+    {
+      tipo: "profesional",
+      titulo: "Law app demo", 
+      descripcion: "A simple legal app powered by Django",
+      url: "https://demo2.codersforyou.es/accounts/login/?next=/",
+      tags: ["Django", "Python", "Cloud"]
+    },
+    
   ]
 }
